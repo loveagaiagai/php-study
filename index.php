33474d003c19f7add27b98bf54cb515321d964bb
loveagaiagai <26787956@qq.com>
2019-11-06 08:41:29 +0800

    第3次提交

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-    echo "Hello";
-    echo "Hello PHP";
+
+function checkNumber($number)
+{
+    if ($number == 5) {
+        throw new Exception("值不能等于5");
+    }
+    return $number;
+}
+
+for ($i = 0; $i < 10; $i++) {
+    try {
+        echo checkNumber($i) . "\r\n";
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
